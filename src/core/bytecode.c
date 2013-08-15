82140424267f610a904129c9decf914e123252e0
Baptiste Covolato <b.covolato@gmail.com>
2013-08-15 15:55:16 +0200

    [CORE] Add _ljit_free_bytecode() function

--- src/core/bytecode.c
+++ src/core/bytecode.c
@@ -1,9 +1,9 @@
 #include <ljit/bytecode.h>
 #include "internal.h"
 
-static ljit_bytecode *ljit_new_bytecode(ljit_bytecode_type type,
-                                        ljit_value op1,
-                                        ljit_value op2)
+static ljit_bytecode *_ljit_new_bytecode(ljit_bytecode_type type,
+                                         ljit_value op1,
+                                         ljit_value op2)
 {
     ljit_bytecode *instr = NULL;
 
@@ -18,6 +18,18 @@ static ljit_bytecode *ljit_new_bytecode(ljit_bytecode_type type,
     return instr;
 }
 
+static void _ljit_free_bytecode(ljit_bytecode *instr)
+{
+    if (!instr)
+        return;
+
+    ljit_free_value(instr->op1);
+    ljit_free_value(instr->op2);
+    ljit_free_value(instr->ret_val);
+
+    free(instr);
+}
+
 ljit_value ljit_inst_get_param(ljit_function *fun, ljit_uchar pos)
 {
     ljit_value ret_val = NULL;
@@ -27,7 +39,7 @@ ljit_value ljit_inst_get_param(ljit_function *fun, ljit_uchar pos)
     if ((pos_cst = ljit_new_uchar_cst(pos)) == NULL)
         return NULL;
 
-    if ((instr = ljit_new_bytecode(GET_PARAM, pos_cst, NULL)) == NULL)
+    if ((instr = _ljit_new_bytecode(GET_PARAM, pos_cst, NULL)) == NULL)
     {
         ljit_free_value(pos_cst);
         return NULL;
@@ -36,7 +48,7 @@ ljit_value ljit_inst_get_param(ljit_function *fun, ljit_uchar pos)
     if ((ret_val = _ljit_new_temporary(fun->signature->ret_type)) == NULL)
     {
         ljit_free_value(pos_cst);
-        ljit_free_bytecode(instr);
+        _ljit_free_bytecode(instr);
         return NULL;
     }
 
