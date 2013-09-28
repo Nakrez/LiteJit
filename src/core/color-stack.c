71237670e4d988bd0b98effb1fd1605e27308d5a
Baptiste Covolato <b.covolato@gmail.com>
2013-09-28 21:12:45 +0200

    [CORE] Add _ljit_color_stack_new()

---
+++ src/core/color-stack.c
@@ -0,0 +1,27 @@
+#include "color-stack.h"
+
+_ljit_color_stack *_ljit_color_stack_new(unsigned int size)
+{
+    _ljit_color_stack *cs = NULL;
+
+    if ((cs = malloc(sizeof (_ljit_color_stack))) == NULL)
+        return NULL;
+
+    if ((cs->edge_number = malloc(size * sizeof (int))) == NULL)
+    {
+        free(cs);
+        return NULL;
+    }
+
+    if ((cs->node_vertex = malloc(size * sizeof (_ljit_liveness_info *))) ==
+        NULL)
+    {
+        free(cs->edge_number);
+        free(cs);
+        return NULL;
+    }
+
+    cs->size = 0;
+
+    return cs;
+}
---
+++ src/core/color-stack.h
@@ -0,0 +1,66 @@
+/*
+    Copyright (C) 2013 Baptiste COVOLATO <b.covolato@gmail.com>
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to
+    deal in the Software without restriction, including without limitation the
+    rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+    sell copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
+*/
+
+/**
+**  @file   color-stack.h
+**  @brief  Stack used in graph coloring
+**  @author Baptiste Covolato <b.covolato@gmail.com>
+**  @date   28 September 2013
+*/
+
+#ifndef COLOR_STACK_H
+# define COLOR_STACK_H
+
+# include "liveness-utils.h"
+
+typedef struct _ljit_color_stack_s _ljit_color_stack;
+
+/*
+**  @brief  This structure is used in the interference graph coloring process
+*/
+struct _ljit_color_stack_s
+{
+    /*
+    **  @brief  The vertex of the edges stored */
+    _ljit_liveness_info **node_vertex;
+
+    /*
+    **  @brief  The edge number in the graph
+    */
+    int *edge_number;
+
+    /*
+    **  @brief  The size of the stack
+    */
+    unsigned int size;
+};
+
+/**
+**  @brief  Allocate a new stack used for graph coloring process
+**  @param  size    The maximum size of the stack
+**
+**  @return The new stack if everything went well, NULL otherwise
+*/
+
+_ljit_color_stack *_ljit_color_stack_new(unsigned int size);
+
+#endif /* !COLOR_STACK_H */
